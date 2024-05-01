1f641a89b5e37b4e3530d86de9feb4d99ec4daa0
Karvekar <159898935+Karvekar@users.noreply.github.com>
2024-05-01 19:37:45 +0530

    Add files via upload

---
+++ A PROGRAM FOR ARITHAATIC OPERATION.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	int a=4,b=2,c;
+	printf("a is %d and b is %d\n",a,b);
+	
+	c=a+b;
+	printf("a+b is %d\n",c);
+	
+	c=a-b;
+	printf("a-b is %d\n",c);
+	
+	c=a*b;
+	printf("a*b is %d\n",c);
+	
+	c=a/b;
+	printf("a/b is %d\n",c);
+	
+	c=a%b;
+	printf("a%%b is %d\n",c);
+
+    return 0;
+
+}
+
+
